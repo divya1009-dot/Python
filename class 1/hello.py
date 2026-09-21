a3f8c6ebbc24435735a6ad48fd88d2cdf64b9877
divya1009-dot <divya1009@googlemail.com>
2026-09-21 18:51:58 +0200

    Add files via upload

---
+++ class 1/hello.py
@@ -0,0 +1,7 @@
+# i am writing a greet
+print("hello my name is Vedant")
+#my hobby 
+print("my hobbies are badminton and taekwondo")
+# my first calculation
+print("the anwswer is 8+5:",8+5)
+print("I like Maths")
